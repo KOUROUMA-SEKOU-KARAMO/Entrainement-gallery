a10a8d54262296ec9a129232ff16ca7c9c76d3be
Sekou Kourouma <sekoukourouma400@gmail.com>
2023-10-09 17:07:12 -0400

    Photos jsonPlaceHolder version 1

--- assets/events/photos.js
+++ assets/events/photos.js
@@ -1,14 +1,43 @@
 const photosContainer=document.querySelector(".photos-container")
+const btnDelete=document.querySelector(".photos-container .btn-delete")
+
+let  photos=[]
 
 
- fetch('https://jsonplaceholder.typicode.com/photos')
- .then((response)=>response.json())
-.then((photos)=>{
-     console.log(photos[0]);
-    //  const imgsAnimal=photos.images.animaux
+ async function photoFetch(){
+     await  fetch('https://jsonplaceholder.typicode.com/photos')
+    .then((response)=>response.json())
+   .then((data)=>{
+            photos=data
+             photos.length=30
+        console.log(photos[0]);
+   
+   })
+ }
 
-    //  photosContainer.innerHTML=`
-    //     <h1>${imgsAnimal.titre} </h1>
-    //  `
+ await photoFetch()
 
-})
+
+  function dispalyPhoto(){
+    photosContainer.innerHTML=photos.map((photo)=>{
+        //    console.log(photo)
+         photos.length=30
+        return `
+           
+                <div class="card">
+                <div class="btn-delete" >X</div>
+
+                        <div class="card-header">
+                                <img src="${photo.url}" alt="">
+                        </div>
+                        <div class="card-body">
+                            <h2 class="title" >${photo.title} </h2>
+                        </div>
+                </div>
+         
+        `
+    }).join("")
+  }
+  dispalyPhoto()
+
+  console.log(btnDelete)
